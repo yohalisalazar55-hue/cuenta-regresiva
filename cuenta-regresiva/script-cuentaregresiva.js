c23ed5193653f5bcea3c6c302c1cb0543885021a
Osneidy <yohalisalazar.55@gmail.com>
2026-09-12 23:31:22 -0300

    Add files via upload

---
+++ cuenta-regresiva/script-cuentaregresiva.js
@@ -0,0 +1,55 @@
+// Fecha de los XV años
+const fechaObjetivo = new Date("2026-10-09T00:00:00");
+
+function actualizarContador() {
+
+    const ahora = new Date();
+
+    const diferencia = fechaObjetivo - ahora;
+
+    if (diferencia <= 0) {
+
+        document.getElementById("dias").textContent = "00";
+        document.getElementById("horas").textContent = "00";
+        document.getElementById("minutos").textContent = "00";
+        document.getElementById("segundos").textContent = "00";
+
+        return;
+    }
+
+    const dias = Math.floor(
+        diferencia / (1000 * 60 * 60 * 24)
+    );
+
+    const horas = Math.floor(
+        (diferencia / (1000 * 60 * 60)) % 24
+    );
+
+    const minutos = Math.floor(
+        (diferencia / (1000 * 60)) % 60
+    );
+
+    const segundos = Math.floor(
+        (diferencia / 1000) % 60
+    );
+
+
+    document.getElementById("dias").textContent =
+        String(dias).padStart(2, "0");
+
+    document.getElementById("horas").textContent =
+        String(horas).padStart(2, "0");
+
+    document.getElementById("minutos").textContent =
+        String(minutos).padStart(2, "0");
+
+    document.getElementById("segundos").textContent =
+        String(segundos).padStart(2, "0");
+}
+
+
+// Mostrar la cuenta inmediatamente
+actualizarContador();
+
+// Actualizar cada segundo
+setInterval(actualizarContador, 1000);
